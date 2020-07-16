82b0d5afa6b4a89184e4436551215600651f15e9
will <sopwill3@gmail.com>
2020-07-16 13:14:24 -0600

    autocomplete works

--- components/HomeView/SearchForm.js
+++ components/HomeView/SearchForm.js
@@ -1,4 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
+const fetch = require('node-fetch');
+
+// Components
 import {View, StyleSheet, Dimensions} from 'react-native';
 import {
   Text,
@@ -11,8 +15,6 @@ import {
   AutocompleteItem,
   Button,
 } from '@ui-kitten/components';
-
-// Components
 
 // Vars
 const studyStatuses = [
@@ -32,8 +34,6 @@ const studyTypes = [
   'Expanded Access',
 ];
 
-// import conditions from './conditions.js';
-
 const vw = Dimensions.get('window').width;
 
 // Functions
@@ -45,7 +45,7 @@ const filter = (item, query) => {
 
 const SearchForm = (props) => {
   const [conditionInput, setConditionInput] = useState('');
-  const [conditionData, setConditionData] = useState(studyStatuses);
+  const [conditionData, setConditionData] = useState(['Heart Attack']);
   const [statusIndex, setStatusIndex] = useState(new IndexPath(0));
   const [typeIndex, setTypeIndex] = useState(new IndexPath(0));
   const [sexInput, setSexInput] = useState('');
@@ -57,19 +57,40 @@ const SearchForm = (props) => {
   const [countryInput, setCountryInput] = useState('');
   const [keywordsInput, setKeywordsInput] = useState('');
 
+  const getAutoFill = async () => {
+    if (conditionInput.length > 0) {
+      const rowsRaw = await fetch(
+        `http://localhost:9000/trial/conditions/${conditionInput}`,
+      );
+      const {data} = await rowsRaw.json();
+      const conditions = [];
+      data.forEach((condition) => {
+        conditions.push(condition.condition);
+      });
+      await setConditionData(conditions);
+      console.log(conditionData);
+    }
+  };
+
+  const requestDataWithDebounce = AwesomeDebouncePromise(getAutoFill, 400);
+
   const createSelectItems = (items) => {
     return items.map((item) => {
       return <SelectItem title={item} key={item} />;
     });
   };
 
-  const onSelect = (index) => {
-    setConditionInput(studyStatuses[index]);
+  const updateData = () => {
+    requestDataWithDebounce();
   };
 
-  const onChangeText = (e) => {
+  const onSelect = (index) => {
+    setConditionInput(conditionData[index]);
+  };
+
+  const onChangeText = async (e) => {
     setConditionInput(e);
-    setConditionData(studyStatuses.filter((item) => filter(item, e)));
+    updateData();
   };
 
   const renderOption = (item, index) => (
